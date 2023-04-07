eeb9b2d293d92680c831a807a96b4d7df72e04e8
张学文 <xuewen.zhang@yintech.cn>
2023-04-07 16:22:26 +0800

    修改

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:chaixiaobu/chaixiaobu.github.io.get master:gh-pages
+git push -f git@github.com:champion-zh/xuewenZhang.github.io.get master:gh-pages
 
 cd -
